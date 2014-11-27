b54c98786ff8a9b5a78a5fef4ccccd27cbb81c00
Trambitskiy Yura <tr.yura@yahoo.com>
2014-11-27 17:55:29 +0300

    began work on game manager

--- classes/Umg_game_manager.php
+++ classes/Umg_game_manager.php
@@ -3,19 +3,39 @@
 class Umg_game_manager {
 
     private $table;
+    private $itemsCount;
 
     function Umg_game_manager($table) {
         $this->table = $table;
+        $this->itemsCount = $table->getNumberOfRows();
     }
 
     public function getMarkup() {
         return "markup";
     }
 
-    public function getRandomItem() {
+    public function getItems() {
 
+        //TODO: this is now completely written!!!
+
+        $indexes = $this->getRandomIndexes();
+        $items = array();
+
+        foreach ($indexes as $index) {
+            //in db indexes start from 1, not 0
+            $id = $index+1;
+            $item = $this->table->getItem($id);
+            array_push($items, $item);
+        }
+
+        return $items;
     }
 
+    public function getRandomIndexes() {
+        $helperArray = array_pad(array(), $this->itemsCount, 0);
+        $indexesArray = array_rand($helperArray, 10);
+        return $indexesArray;
+    }
 
 
 }
