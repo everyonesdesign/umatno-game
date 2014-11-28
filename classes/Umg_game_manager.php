47a98f1b9ae77423e13974bca432d5decd9aa931
Trambitskiy Yura <tr.yura@yahoo.com>
2014-11-28 18:20:28 +0300

    began to write variants adding function

--- classes/Umg_game_manager.php
+++ classes/Umg_game_manager.php
@@ -11,29 +11,54 @@ class Umg_game_manager {
     }
 
     public function getMarkup() {
-
         //FIXME: for test
-        return $this->itemsCount;
+        $questions = $this->getQuestions();
+//        $questions_with_variants = $this->addVariantsToQuestions($questions);
+        return var_dump($questions);
     }
 
-    public function addVariantsToQuestion() {
+    public function generateQuestionsEntities() {
 
     }
 
-    public function getQuestionItems() {
-        $indexes = $this->getRandomIndexes();
-        $items = array();
+    public function addVariantsToQuestions($questions) {
 
+        foreach ($questions as $question) {
+            $id = $question[0]["id"];
+
+            $variants = getRandomVariants($id);
+
+            $question["variants"] = $variants;
+
+            function getRandomVariants($id) {
+                $helperArray = array_pad(array(), $this->itemsCount, 0);
+                $indexesArray = array_rand($helperArray, 3);
+                //if same id recursive call again
+                if (in_array($id, $indexesArray)) {
+                    getRandomVariants($id);
+                } else {
+                    return $indexesArray;
+                }
+            }
+
+        }
+
+        return $questions;
+
+    }
+
+    public function getQuestions() {
+        $indexes = $this->getQuestionsRandomIndexes();
+        $questions = array();
         foreach ($indexes as $index) {
             $id = $index+1;
             $item = $this->table->getItem($id);
-            array_push($items, $item);
+            array_push($questions, $item);
         }
-
-        return $items;
+        return $questions;
     }
 
-    public function getRandomIndexes() {
+    public function getQuestionsRandomIndexes() {
         $helperArray = array_pad(array(), $this->itemsCount, 0);
         $indexesArray = array_rand($helperArray, 10);
         return $indexesArray;
