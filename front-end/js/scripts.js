3b5eb6b2ff43700912d7fce1ea05d48da7abecf5
Trambitskiy Yura <tr.yura@yahoo.com>
2014-11-28 13:49:54 +0300

    created basic angular navigation

--- front-end/js/scripts.js
+++ front-end/js/scripts.js
@@ -1,6 +1,12 @@
 $(function() {
-   $(".umg_body-answerItem input").styler();
+//   $(".umg_body-answerItem input").styler();
 });
 
 var game = angular.module("game", []);
 
+game.value("data", umg_data);
+
+game.controller("gameController", ["data", function(data) {
+    this.questionNumber = 0;
+    this.questions = data.questions;
+}]);
