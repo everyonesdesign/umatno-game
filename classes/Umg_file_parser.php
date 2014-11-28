86af614b05d7a8242af2099548b1b41acfa406a2
Yura Trambitskiy <tr.yura@yahoo.com>
2014-11-29 01:58:29 +0300

    fixed last semicolor parsing issue with regex

--- classes/Umg_file_parser.php
+++ classes/Umg_file_parser.php
@@ -21,11 +21,11 @@ class Umg_file_parser {
         foreach($file_content as $line) {
             if ($line_type == 0) {
                 $name = str_replace("@@-@@", "", $line);
-                $name = str_replace("\n", "", $name);
+                $name = str_replace(";?\n", "", $name);
                 $line_type++;
             } elseif ($line_type == 1) {
                 $value = str_replace("@@-@@", "", $line);
-                $value = str_replace("\n", "", $value);
+                $value = str_replace(";?\n", "", $value);
                 $line_type++;
             } elseif ($line_type == 2) {
                 $table->setItem($name, $value);
