1cb6ac268eee5437964648eddd96320e36af6862
Trambitskiy Yura <tr.yura@yahoo.com>
2014-11-28 14:45:06 +0300

    wrote the controller so thr results are actually counted

--- front-end/js/scripts.js
+++ front-end/js/scripts.js
@@ -6,7 +6,50 @@ var game = angular.module("game", []);
 
 game.value("data", umg_data);
 
-game.controller("gameController", ["data", function(data) {
-    this.questionNumber = 0;
-    this.questions = data.questions;
+game.controller("gameController", ["$scope", "data", function($scope, data) {
+
+    var $this = this;
+    $this.questionNumber = 0;
+    $this.questions = data.questions;
+    $this.results = {};
+
+    $this.nextStep = function() {
+        if ($this.questions[$this.questionNumber].userAnswer==undefined) {
+            return;
+        }
+        $this.questionNumber+=1;
+    };
+
+    $this.countResults = function() {
+        if ($this.questions[$this.questionNumber].userAnswer==undefined) {
+            return;
+        }
+        for (var i=0; i<$this.questions.length; i++) {
+            for (var j=0; j<$this.questions[i].answers.length; j++) {
+                if (""+CryptoJS.SHA1($this.questions[i].answers[j]) == $this.questions[i].hash) {
+                    $this.questions[i].correct = j;
+                }
+            }
+        }
+    };
+
 }]);
+
+game.filter("correct", function() {
+    return function(input) {
+        var result = [];
+        for (var i=0; i<input.length; i++) {
+            if (input[i].correct === input[i].userAnswer) {
+                result.push(input[i]);
+            }
+        }
+        console.dir(result);
+        return result;
+    }
+});
+
+game.filter("join", function() {
+    return function(input) {
+        return input.join(", ");
+    }
+});
