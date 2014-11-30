822c5c61220c70b8b20800dcb53a075e0bbf0d94
Yura Trambitskiy <tr.yura@yahoo.com>
2014-11-30 11:45:33 +0300

    removed conditions for films/actors division

--- umg.php
+++ umg.php
@@ -20,8 +20,8 @@ require_once(plugin_dir_path( __FILE__ )."classes/Umg_questions_manager.php");
 //creating instance of table for game 'guess film by actors list'
 $films_table_name = $wpdb->prefix.'umg_films';
 $films_file = plugin_dir_path( __FILE__ ).'data/films-data.txt';
-global $films_table;
-$films_table = new Umg_table_manager($films_table_name, $films_file);
+global $umg_films_table;
+$umg_films_table = new Umg_table_manager($films_table_name, $films_file);
 
 
 /*
@@ -32,16 +32,16 @@ $films_table = new Umg_table_manager($films_table_name, $films_file);
 
 //creating a db table if there's no
 function umg_install() {
-    global $films_table;
-    $films_table->createTable();
+    global $umg_films_table;
+    $umg_films_table->createTable();
 }
 register_activation_hook( __FILE__, 'umg_install' );
 
 //parse file and push it's data to the database
 function umg_parse_data() {
-    global $films_table;
-    $films_table->resetTable();
-    $films_table->parseFile();
+    global $umg_films_table;
+    $umg_films_table->resetTable();
+    $umg_films_table->parseFile();
 }
 register_activation_hook( __FILE__, 'umg_parse_data' );
 
@@ -65,16 +65,18 @@ add_action('wp_enqueue_scripts', 'umg_enqueue_files');
  */
 
 function umg_activate_game($attrs) {
-    if ($attrs['name'] == "films") {
-        global $films_table;
-        $game = new Umg_questions_manager($films_table);
+    /*if ($attrs['name'] == "films") {
+        global $umg_films_table;
+        $game = new Umg_questions_manager($umg_films_table);
     } elseif ($attrs['name'] == "actors") {
-        //TODO: write this
-    }
+        //TODO: write game with best actors
+    }*/
+    global $umg_films_table;
+    $game = new Umg_questions_manager($umg_films_table);
     $markup = $game->getMarkup();
     return $markup;
 }
-add_shortcode('game', 'umg_activate_game');
+add_shortcode('umg_game', 'umg_activate_game');
 
 
 
